fc18775e42a147e241e26f165e04a1d872aab970
Elmi Rabelo <rabeloelmi@gmail.com>
2019-07-06 19:42:12 -0300

    Funcionalidade de menu responsivo com javascript criada

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+function burguerMenu(){
+  const burguer = document.querySelector('.burguer');
+  const navLinks = document.querySelector('.nav-links');
+
+  burguer.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+  });
+}
+
+burguerMenu();
